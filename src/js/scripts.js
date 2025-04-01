eb55fa1df4a04c892b18b53620e299043abd7c68
marinaxu99 <mingnan@bu.edu>
2025-04-01 02:04:34 -0400

    04/01 Homepage Setup Done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,17 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+//for thought bubble changing on the homepage, gotten from GPT, prompt: how to make an img change into another after 1 second of delay
+const img = document.querySelector('.thought-bubble');
+setTimeout(() => {
+	img.src = 'images/thought-bubble-2.svg';
+}, 2000);
+
+//make the menu container appear when the robot is clicked
+const robot = document.querySelector('.robot-click');
+const popup = document.querySelector('.menu-container');
+
+robot.addEventListener('click', () => {
+	popup.classList.remove('hidden');
+});
